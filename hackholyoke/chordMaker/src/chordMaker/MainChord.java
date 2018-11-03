90cbe34d4a2ba30954673ae0d5e4386cb3b40c9a
Ye Won Hong <33376919+hyw1397@users.noreply.github.com>
2018-11-03 15:14:23 -0400

    hackholyoke2018 chordMaker

---
+++ hackholyoke/chordMaker/src/chordMaker/MainChord.java
@@ -0,0 +1,25 @@
+package chordMaker;
+
+import java.awt.AWTEventMulticaster;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+import java.awt.*;
+
+import javax.swing.plaf.basic.BasicTreeUI;
+
+public class MainChord {
+	
+	public static void main(String[] args) {
+		System.out.println("Input your melody! Type 'enter' when you're done!");
+		//KeyListener listener = new BasicTreeUI.KeyHandler();
+		//while(!listener.keyPressed(KeyEvent.VK_ENTER)) {
+			
+		//}
+	}
+
+
+
+    
+}
